7845b3901ebf3034470b253393044368e9199e5c
zethra <jediben97@gmail.com>
2015-03-12 12:31:54 -0400

    Added sperate main page that initazlizes others

--- app/src/main/java/com/team3637/pitscoutingappb/MainActivity.java
+++ app/src/main/java/com/team3637/pitscoutingappb/MainActivity.java
@@ -1,9 +1,11 @@
 package com.team3637.pitscoutingappb;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick (View view) {
+        switch (view.getId()) {
+            case R.id.createButton:
+                Intent createButton = new Intent(this, DataEntryActivity.class);
+                startActivity(createButton);
+                break;
+            case R.id.listButton:
+                Intent listButton = new Intent(this, RobotListActivity.class);
+                startActivity(listButton);
+                break;
+        }
     }
 }
